ff30389957dbf86d65063743de1cd971b3f777f6
ian13456 <21ianh1@tas.tw>
2020-03-16 12:28:40 +0800

    feat: forgot password

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -6,41 +6,85 @@
     </div>
     <div class="form-group">
       <label for="password">Password</label>
-      <input type="password" class="form-control" id="password" v-model="password" required />
+      <input
+        type="password"
+        class="form-control"
+        id="password"
+        v-model="password"
+        required
+      />
     </div>
     <div class="form-group">
       <span>
         Don't have an account?
-        <a href="/signup">Sign up</a> here
+        <a href="/signup">Sign up</a> here.
+        <a v-on:click="forgotPassword">Forgot your password?</a>
       </span>
     </div>
     <button type="submit" class="btn btn-primary w-100">Login</button>
     <a href="/">
-      <div class="btn btn-primary w-100" style="background-color:#db8261;margin-top:10px">Go Back</div>
+      <div class="btn btn-primary w-100" style="background-color:#db8261;margin-top:10px">
+        Go Back
+      </div>
     </a>
   </form>
 </template>
 
 <script>
 export default {
-  name: "LoginForm",
+  name: 'LoginForm',
   data() {
     return {
       email: null,
       password: null
-    };
+    }
   },
   methods: {
+    forgotPassword() {
+      this.$swal
+        .fire({
+          icon: 'question',
+          title: 'Enter your account email',
+          text: "Enter your email and we'll send you a link to reset your password.",
+          input: 'email',
+          inputAttributes: {
+            autocapitalize: 'off'
+          },
+          showCancelButton: true,
+          confirmButtonText: 'Send',
+          showLoaderOnConfirm: true,
+          preConfirm: value => {
+            console.log(value)
+            this.axios
+              .post('user/reset', { email: value })
+              .then(() => {
+                return ''
+              })
+              .catch(error => {
+                this.$swal.showValidationMessage(`Request failed: ${error}`)
+              })
+          },
+          allowOutsideClick: () => !this.$swal.isLoading()
+        })
+        .then(({ dismiss }) => {
+          if (!dismiss)
+            this.$swal.fire({
+              icon: 'success',
+              title: `We've sent you an email!`,
+              text: 'Check your inbox for a link to reset your password.'
+            })
+        })
+    },
     onSubmit() {
       let formData = {
         email: this.email,
         password: this.password
-      };
-      this.$swal.showLoading();
-      this.$emit("form-submitted", formData);
-      this.email = null;
-      this.password = null;
+      }
+      this.$swal.showLoading()
+      this.$emit('form-submitted', formData)
+      this.email = null
+      this.password = null
     }
   }
-};
+}
 </script>
